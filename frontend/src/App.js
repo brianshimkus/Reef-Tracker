59a50c0010bf7cbde03f109fccda1a3707cd2be3
Brian Shimkus <brianshimkus1@gmail.com>
2022-04-07 23:50:21 -0500

    Made HTTP request from React to Node

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react'
+
 function App() {
+	const [data, setData] = useState(null)
+
+	useEffect(() => {
+		fetch('/api')
+			.then((res) => res.json())
+			.then((data) => setData(data.message))
+	}, [])
+
 	return (
 		<div className='App'>
-			<h1>Hello</h1>
+			<header>
+				<p>{!data ? 'Loading...' : data}</p>
+			</header>
 		</div>
 	)
 }
